985676600ad31268b038ad51dd16c2e8fca0e780
macbookair <dailydevo9@gmail.com>
2022-03-21 22:21:53 +0100

    first commit

---
+++ assets/js/fun-fact-counter.js
@@ -0,0 +1,14 @@
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    // Counter Up by Odometer
+    setTimeout(function () {
+      activeMember.innerHTML = 300;
+      totalDeposite.innerHTML = 6;
+      totalWithdraw.innerHTML = 9;
+      totlaLocation.innerHTML = 36;
+    }, 1000);
+    // Counter Up by Odometer End
+  });
+})(jQuery);
